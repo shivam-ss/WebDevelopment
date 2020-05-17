165d418246c6eaef0469091c716ae9aea81f6d34
shivam-ss <shivamsrivastava800@gmail.com>
2020-05-17 16:13:24 +0530

    For each loop on mongoose

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -8,9 +8,9 @@ const fruitschema= new mongoose.Schema({
     review: String
 });  // structure of collection
 // Fruit has to follow thae fruitschema rule
-const Fruit=mongoose.model("Fruit", fruitschema);
+const Fruit=mongoose.model("Fruit", fruitschema); // means Fruit schema will follow above structure, uses low dashing to pluralize Fruit collection to Fruits
 
-const fruit=new Fruit({
+const fruit=new Fruit({       //inserting new record on a schema whose structure follows the Fruit const aboce
     name: "Apple",
     rating: 10,
     review: "Very good"
@@ -33,3 +33,39 @@ const person= new Person({
 
 person.save().then(()=>console.log("saved shivam"))
 // create just many objects and call fruit.insertMany([a,b,c])
+
+//like: 
+/* fruits.insertMany([apple,orange,banana],function(err){
+    console.log(err);
+})
+
+*/
+
+// to show records:
+Fruit.find(function(err,fruits){
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(fruits);
+    }
+
+    
+}
+);
+
+
+//using for each loop to get only name of fruits
+Fruit.find(function(err,fruits){
+    if(err){
+        console.log(err);
+    }
+    else{
+        fruits.forEach(function(fruit){
+            console.log(fruit.name);
+        })
+    }
+
+    
+}
+);
